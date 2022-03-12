3fe445a90a454eb061db0d56ce5f05b7343a4c03
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-03-12 12:59:29 -0500

    initial commit

---
+++ Tipsy-iOS13/Tipsy-iOS13/Tipsy/Controller/ResultsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by user202406 on 3/12/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var settingsLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
